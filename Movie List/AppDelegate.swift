f7e91e582dd42acbc4bef105e458c288077af818
Bree <bree.j.32@gmail.com>
2019-09-27 23:43:22 -0400

    adding tableview and navigation

--- Movie List/AppDelegate.swift
+++ Movie List/AppDelegate.swift
@@ -6,5 +6,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+        
+        let movieList = UserViewController(nibName: "UserViewController", bundle: nil)
+            movieList.title = "Favorite Movies"
+            return true
+            
+            let navigationController = UINavigationController(rootViewController: movieList)
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        
+       override func numberOfSections(in tableView: UITableView) -> Int
+        {
+            return 1
+        }
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return movieList.count
+        }
+        }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+
+    }
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+
+
+    cell.textLabel?.text = movieList[indexPath.row][0],
+    
+     print("\(#function) --- section = \(indexPath.section), row = \(indexPath.row)")
+
+    return cell
+}
+
+
+
+class Movie {
+    var movieName: String
+    var hasSeen: Bool
+    
+    init(movieName: String, hasSeen: Bool) {
+        self.movieName = movieName
+        self.hasSeen = false
     }
 }
